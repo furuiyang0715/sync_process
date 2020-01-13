f11820132e86e86f998d1fbc54dd383b13fee369
furuiyang <furuiyang0715@gmail.com>
2020-01-13 10:04:29 +0800

    启动定时任务

--- jz_index/main.py
+++ jz_index/main.py
@@ -1,7 +1,40 @@
+import functools
+import logging
+import time
+
+import schedule
+from raven import Client
+
+from jz_index.configs import SENTRY_DSN
 from jz_index.sync_index import IndexSync
 
 
-def main():
+logger = logging.getLogger("index_log")
+
+sentry = Client(SENTRY_DSN)
+
+
+def catch_exceptions(cancel_on_failure=False):
+    # 定时任务中的异常捕获
+    def catch_exceptions_decorator(job_func):
+        @functools.wraps(job_func)
+        def wrapper(*args, **kwargs):
+            try:
+                return job_func(*args, **kwargs)
+            except:
+                import traceback
+                logger.warning(traceback.format_exc())
+                sentry.captureException(exc_info=True)
+                if cancel_on_failure:
+                    # print(schedule.CancelJob)
+                    # schedule.cancel_job()
+                    return schedule.CancelJob
+        return wrapper
+    return catch_exceptions_decorator
+
+
+@catch_exceptions(cancel_on_failure=True)
+def task():
     """
     定时任务函数 每天执行一次 当前类需要每天实例化一次
     :return:
@@ -9,6 +42,15 @@ def main():
     runner = IndexSync()
     runner.index_run()
 
+
+def main():
+    schedule.every().day.at("17:30").do(task)
+
+    while True:
+        logger.info(schedule.jobs)
+        schedule.run_pending()
+        time.sleep(300)
+        logger.info("no work to do, waiting ...")
 
 
 main()
