4c53663aa15a0304725e86d75312471bfd613685
furuiyang <furuiyang0715@gmail.com>
2020-01-13 10:36:29 +0800

    little

--- jz_index/base_index.py
+++ jz_index/base_index.py
@@ -85,4 +85,5 @@ class BaseSync(object):
         return cli
 
     def log(self, some):
-        print("====== {} ======".format(some))
+        # print("====== {} ======".format(some))
+        pass
